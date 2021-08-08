5be02f0c146469ef49b0eaec740fc336bd7e69f0
Juan Porrino <jporrino@clapps.xyz>
2021-08-08 17:29:00 -0300

    starred background

--- src/app/main/main-component/main.component.ts
+++ src/app/main/main-component/main.component.ts
@@ -1,30 +1,101 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import * as Vara from 'vara';
+import {fadeInOnEnterAnimation} from 'angular-animations';
 
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+      fadeInOnEnterAnimation({duration: 10000})
+  ]
 })
 export class MainComponent implements OnInit{
+  innerWidth;
+  innerHeight;
+  varaIsDone = false;
+
+  tinyStars = [];
+  mediumStars = [];
+  bigStars = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+
+    this.initStars();
+
     this.signature();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+    this.initStars();
+  }
+
   signature() {
-    let vara = new Vara('#signature', '../assets/fonts/parisienne.json', [
+    const vara = new Vara('#signature', '../assets/fonts/parisienne.json', [
       {
         text: 'Lau y Juan',
         duration: 3000,
         textAlign: 'center'
       },
     ], {
-      fontSize: 72,
+      fontSize: this.innerWidth < 600 ? 48 : 72,
       color: '#FAFAFA',
     });
+    vara.animationEnd(() => {
+      this.varaIsDone = true;
+    });
+  }
+
+  initStars(): void{
+    this.tinyStars = [];
+    this.mediumStars = [];
+    this.bigStars = [];
+
+    let tinyStarsAmount = 700;
+    let mediumStarsAmount = 200;
+    let bigStarsAmount = 100;
+
+    if (this.innerWidth < 600) {
+      tinyStarsAmount = 233;
+      mediumStarsAmount = 66;
+      bigStarsAmount = 33;
+    }
+
+    for (let i = 0; i < tinyStarsAmount; i++) {
+      this.tinyStars.push({
+        top: this.randomHeight(),
+        right: this.randomWidth(),
+      });
+    }
+
+    for (let i = 0; i < mediumStarsAmount; i++) {
+      this.mediumStars.push({
+        top: this.randomHeight(),
+        right: this.randomWidth(),
+      });
+    }
+
+    for (let i = 0; i < bigStarsAmount; i++) {
+      this.bigStars.push({
+        top: this.randomHeight(),
+        right: this.randomWidth(),
+      });
+    }
+  }
+
+  randomHeight(): string{
+    return `${Math.floor(Math.random() * (this.innerHeight - 5))}px`;
+  }
+
+  randomWidth(): string{
+    return `${Math.floor(Math.random() * (this.innerWidth - 5))}px`;
   }
 
 }
--- src/app/main/main-component/main.module.ts
+++ src/app/main/main-component/main.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { MainComponent } from './main.component';
+import {NgModule} from '@angular/core';
+import {MainComponent} from './main.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {CommonModule} from '@angular/common';
 
 const routes: Routes = [
-  {
-    path     : '',
-    component: MainComponent,
-  }
+    {
+        path: '',
+        component: MainComponent,
+    }
 ];
 
 @NgModule({
-  declarations: [
-    MainComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    MatProgressSpinnerModule,
-  ],
-  providers: [],
+    declarations: [
+        MainComponent,
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        MatProgressSpinnerModule,
+    ],
+    providers: [],
 })
-export class MainModule { }
+export class MainModule {
+}
